3e022f2d1cfca516b7a37e182b40038fe151246f
Joy Zhou <zhou1559@umn.edu>
2023-02-16 14:30:21 -0600

    Data imported

--- R/week5.R
+++ R/week5.R
@@ -2,3 +2,10 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
 library(lubridate)
+
+# Data Import
+Adata_tbl <- read_delim(file = "../data/Aparticipants.dat", delim = "-", col_names = c("casenum","parnum","stimver","datadate","qs"))
+Anotes_tbl <- read_delim(file = "../data/Anotes.csv", delim = ",")
+Bdata_tbl <- read_delim(file = "../data/Bparticipants.dat", delim = "\t", col_names = c("casenum","parnum","stimver","datadate",paste0("q", 1:10)))
+Bnotes_tbl <- read_delim(file = "../data/Bnotes.txt", delim = "\t")
+
