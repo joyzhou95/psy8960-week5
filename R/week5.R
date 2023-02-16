7e92e4a23970009aaae187998a3340e299477ff2
Joy Zhou <zhou1559@umn.edu>
2023-02-16 15:24:07 -0600

    Notes column added to Aclean_tbl

--- R/week5.R
+++ R/week5.R
@@ -13,5 +13,7 @@ Bnotes_tbl <- read_delim(file = "../data/Bnotes.txt", delim = "\t")
 Aclean_tbl <- Adata_tbl %>%
   separate(qs, into = paste0("q", 1:5)) %>%
   mutate(datadate = as.POSIXct(datadate, format = "%b %d %Y, %H:%M:%S")) %>%
-  mutate(q1 = as.integer(q1), q2 = as.integer(q2), q3 = as.integer(q3), q4 = as.integer(q4), q5 = as.integer(q5))
+  mutate(q1 = as.integer(q1), q2 = as.integer(q2), q3 = as.integer(q3), q4 = as.integer(q4), q5 = as.integer(q5)) %>%
+  inner_join(Anotes_tbl, by = "parnum")
+  
   
