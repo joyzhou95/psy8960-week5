f0b618997d8cffce538bee7c69fa35b0b0a07c7e
Joy Zhou <zhou1559@umn.edu>
2023-02-16 15:01:53 -0600

    Changed response data type

--- R/week5.R
+++ R/week5.R
@@ -12,4 +12,6 @@ Bnotes_tbl <- read_delim(file = "../data/Bnotes.txt", delim = "\t")
 # Data Cleaning
 Aclean_tbl <- Adata_tbl %>%
   separate(qs, into = paste0("q", 1:5)) %>%
-  mutate(datadate = as.POSIXct(datadate, format = "%b %d %Y, %H:%M:%S"))
+  mutate(datadate = as.POSIXct(datadate, format = "%b %d %Y, %H:%M:%S")) %>%
+  mutate(q1 = as.integer(q1), q2 = as.integer(q2), q3 = as.integer(q3), q4 = as.integer(q4), q5 = as.integer(q5))
+  
