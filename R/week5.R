835bb11f61d636ca32246c7739187b0ce7051431
Joy Zhou <zhou1559@umn.edu>
2023-02-17 23:13:43 -0600

    Corrected the order of the last two lines and used lubridate for dates conversion

--- R/week5.R
+++ R/week5.R
@@ -12,15 +12,15 @@ Bnotes_tbl <- read_delim(file = "../data/Bnotes.txt", delim = "\t")
 # Data Cleaning
 Aclean_tbl <- Adata_tbl %>%
   separate(qs, into = paste0("q", 1:5)) %>%
-  mutate(datadate = as.POSIXct(datadate, format = "%b %d %Y, %H:%M:%S")) %>%
+  mutate(datadate = mdy_hms(datadate)) %>%
   mutate(q1 = as.integer(q1), q2 = as.integer(q2), q3 = as.integer(q3), q4 = as.integer(q4), q5 = as.integer(q5)) %>%
   inner_join(Anotes_tbl, by = "parnum") %>%
   filter(is.na(notes))
 ABclean_tbl <- Bdata_tbl %>%
-  mutate(datadate = as.POSIXct(datadate, format = "%b %d %Y, %H:%M:%S")) %>%
+  mutate(datadate = mdy_hms(datadate)) %>%
   mutate(q1 = as.integer(q1), q2 = as.integer(q2), q3 = as.integer(q3), q4 = as.integer(q4), q5 = as.integer(q5), q6 = as.integer(q6), q7 = as.integer(q7), q8 = as.integer(q8), q9 = as.integer(q9), q10 = as.integer(q10)) %>%
   inner_join(Bnotes_tbl, by = "parnum") %>%
   filter(is.na(notes)) %>%
-  bind_rows(Aclean_tbl, .id = "lab") %>%
-  select(-notes)
+  select(-notes) %>%
+  bind_rows(Aclean_tbl, .id = "lab")
   
